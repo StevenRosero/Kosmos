6a24fdcfa32586bee63ed8b983da693abeea7a5b
steve <steven.ros.cap@gmail.com>
2019-06-02 06:00:49 +0100

    Log In, register, and Home created,olso main functions to conect with firebase. Some issues at log in -- it has to check

---
+++ app/src/main/java/com/fct/kosmos/HomeLogInOk.java
@@ -0,0 +1,143 @@
+package com.fct.kosmos;
+
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class HomeLogInOk extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    FirebaseUser usuarioActual;
+    private FirebaseAuth mAuth;
+
+    TextView navNombreUsuario;
+    TextView navUsuarioEmail;
+    ImageView navUsuarioImagen;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_log_in_ok);
+
+        updateNavCabecero();
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //Init Firebase
+        mAuth = FirebaseAuth.getInstance();
+        usuarioActual = mAuth.getCurrentUser();
+    }
+    private void updateNavCabecero() {
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        View actualizarCabecero = navigationView.getHeaderView(0);
+
+        navNombreUsuario = actualizarCabecero.findViewById(R.id.navUsuarioNombre);
+        navUsuarioEmail = actualizarCabecero.findViewById(R.id.navUsuarioEmail);
+        navUsuarioImagen = actualizarCabecero.findViewById(R.id.navUsuarioImgPerfil);
+
+
+        navNombreUsuario.setText(usuarioActual.getDisplayName());
+        navUsuarioEmail.setText(usuarioActual.getEmail());
+        Glide.with(this).load(usuarioActual.getPhotoUrl()).into(navUsuarioImagen);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_log_in_ok, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
--- app/src/main/java/com/fct/kosmos/activities/LoginActivity.java
+++ app/src/main/java/com/fct/kosmos/activities/LoginActivity.java
@@ -1,33 +1,146 @@
 package com.fct.kosmos.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.fct.kosmos.HomeLogInOk;
 import com.fct.kosmos.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
 
-    private Button Boton;
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private EditText nombreUsuario, contraseña;
+    private Button btnAcceso, btnRegistro;
+
+    //Conexion a firebase
+    private FirebaseAuth mAuth;
+
+    //lOG IN GOOGLE
+    private GoogleApiClient googleApi;
+    public static final int SING_IN_CODE = 777;
+
+    private Intent activityRegistro,restart, homeActivity;
+    Toast toast;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Boton = (Button) findViewById(R.id.button2);
+        //asignar los valores de las variables
+        nombreUsuario = findViewById(R.id.etId);
+        contraseña = findViewById(R.id.etContrasenia);
+        btnAcceso = findViewById(R.id.btnAcceso);
+        btnRegistro = findViewById(R.id.btnAcceso2);
 
-        Boton.setOnClickListener(new View.OnClickListener() {
+        activityRegistro = new Intent(this, RegisterActivity.class);
+        homeActivity = new Intent(this, HomeLogInOk.class);
+        restart = new Intent(this, LoginActivity.class);
+
+        //gOOGLE
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApi = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        btnAcceso.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegistreActivity.class);
-                startActivity(intent);
-                finish();
+            public void onClick(View view) {
+                //barraProgreso.setVisibility(View.VISIBLE);
+                btnAcceso.setVisibility(View.INVISIBLE);
+                btnRegistro.setVisibility(View.INVISIBLE);
+                //missPass.setVisibility(View.INVISIBLE);
+
+
+                final String idUsuario = nombreUsuario.getText().toString();
+                final String contraseniaUsuario = contraseña.getText().toString();
+
+                if (idUsuario.isEmpty() || contraseniaUsuario.isEmpty()) {
+                    showMessage("Verifica los datos introducidos");
+                    //barraProgreso.setVisibility(View.INVISIBLE);
+                    btnAcceso.setVisibility(View.VISIBLE);
+                    btnRegistro.setVisibility(View.VISIBLE);
+                } else {
+                    acceder(idUsuario, contraseniaUsuario);
+                }
+
+
             }
         });
     }
 
+
+    private void acceder(String idUsuario, String contraseniaUsuario) {
+
+        mAuth.signInWithEmailAndPassword(idUsuario, contraseniaUsuario).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //barraProgreso.setVisibility(View.VISIBLE);
+                    btnAcceso.setVisibility(View.INVISIBLE);
+                    btnRegistro.setVisibility(View.INVISIBLE);
+                    actualizarUsuario();
+                }else{
+                    showMessage(task.getException().getMessage());
+                    iniciarDeNuevo();
+                }
+
+
+            }
+        });
+    }
+    private void updateUI(GoogleSignInAccount account) {
+
+    }
+
+    private void actualizarUsuario() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        startActivity(homeActivity);
+        finish();
+    }
+
+    private void showMessage(String s) {
+        if (toast != null)
+            toast.cancel();
+        toast = Toast.makeText(this, "No hay una cuenta asociada a este email", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void accesoRegistro(View v){
+        startActivity(activityRegistro);
+    }
+
+
+    private void iniciarDeNuevo() {
+        startActivity(homeActivity);
+        finish();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
