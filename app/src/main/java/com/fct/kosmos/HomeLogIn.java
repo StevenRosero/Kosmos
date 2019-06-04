39f35d1a9c6b6f16afe8971b2dfe0811f8a0f03f
sroseroc <steven.ros.cap@gmail.com>
2019-06-04 12:57:07 +0200

    Log In + Register + Home perfectly working

--- app/src/main/java/com/fct/kosmos/HomeLogIn.java
+++ app/src/main/java/com/fct/kosmos/HomeLogIn.java
@@ -16,22 +16,8 @@ import android.view.Menu;
 
 
 import android.content.Intent;
-import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
-
-
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-import android.view.MenuItem;
 
 import com.fct.kosmos.activities.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
